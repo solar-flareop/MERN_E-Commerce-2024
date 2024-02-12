9755747fb03f1e17a99e66e94b4e5c5425d5b8dc
Suraj Pattade <surajpattade@gmail.com>
2024-02-12 21:56:33 +0530

    Added admin dashboards & ready to deploy

--- frontend/src/redux/api/userAPI.ts
+++ frontend/src/redux/api/userAPI.ts
@@ -1,5 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { MessageResponse, UserResponse } from "../../types/api-types";
+import {
+  AllUsersResponse,
+  DeleteUserRequest,
+  MessageResponse,
+  UserResponse,
+} from "../../types/api-types";
 import { User } from "../../types/types";
 import axios from "axios";
 
@@ -8,6 +13,7 @@ const SERVER = import.meta.env.VITE_SERVER;
 export const userAPI = createApi({
   reducerPath: "userAPI",
   baseQuery: fetchBaseQuery({ baseUrl: `${SERVER}/api/v1/user/` }),
+  tagTypes: ["users"],
   endpoints: (builder) => ({
     login: builder.mutation<MessageResponse, User>({
       query: (user) => ({
@@ -15,6 +21,20 @@ export const userAPI = createApi({
         method: "POST",
         body: user,
       }),
+      invalidatesTags: ["users"],
+    }),
+
+    allUsers: builder.query<AllUsersResponse, string>({
+      query: (id) => `all?id=${id}`,
+      providesTags: ["users"],
+    }),
+
+    deleteUser: builder.mutation<MessageResponse, DeleteUserRequest>({
+      query: ({ userId, adminUserId }) => ({
+        url: `${userId}?id=${adminUserId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["users"],
     }),
   }),
 });
@@ -29,4 +49,5 @@ export const getUser = async (id: string) => {
     console.log("getUser", error);
   }
 };
-export const { useLoginMutation } = userAPI;
+export const { useLoginMutation, useAllUsersQuery, useDeleteUserMutation } =
+  userAPI;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -3,6 +3,8 @@ import { productAPI } from "./api/productAPI";
 import { userAPI } from "./api/userAPI";
 import { userReducer } from "./reducer/userReducer";
 import { cartReducer } from "./reducer/cartReducer";
+import { orderAPI } from "./api/orderAPI";
+import { dashBoardAPI } from "./api/dashBoardAPI";
 
 export const SERVER = import.meta.env.VITE_SERVER;
 
@@ -10,10 +12,18 @@ export const store = configureStore({
   reducer: {
     [userAPI.reducerPath]: userAPI.reducer,
     [productAPI.reducerPath]: productAPI.reducer,
+    [orderAPI.reducerPath]: orderAPI.reducer,
+    [dashBoardAPI.reducerPath]: dashBoardAPI.reducer,
     [userReducer.name]: userReducer.reducer,
     [cartReducer.name]: cartReducer.reducer,
   },
-  middleware: (mid) => [...mid(), userAPI.middleware, productAPI.middleware],
+  middleware: (mid) => [
+    ...mid(),
+    userAPI.middleware,
+    productAPI.middleware,
+    orderAPI.middleware,
+    dashBoardAPI.middleware,
+  ],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
